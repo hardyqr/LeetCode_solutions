053992a023f1dbc50f9e75ca9b4b35f3837a057a
hardyqr <Fangyu@FangyumatoMacBook-Air.local>
2016-10-07 22:06:49 +0800

    ~

--- load&write_csv.py
+++ load&write_csv.py
@@ -31,3 +31,41 @@ for line in reader:
     print line
 
 csvfile.close()
+
+#3.读取txt
+f = open('test.txt', 'r')
+print f.read()
+
+f.seek(0)
+print f.read(14)
+
+f.seek(0)
+print f.readline()
+print f.readline()
+
+f.seek(0)
+print f.readlines()
+
+f.seek(0)
+for line in f:
+    print line,
+
+f.close()
+#4.写入txt
+
+f = open('test.txt', 'r+')
+f.truncate()
+f.write('0123456789abcd')
+
+f.seek(3)
+print f.read(1)
+print f.read(2)
+print f.tell()
+
+f.seek(3, 1)
+print f.read(1)
+
+f.seek(-3, 2)
+print f.read(1)
+
+f.close()
