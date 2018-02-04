4466553d417371118eb63d0ef903ab3e795026ef
hardyqr <liufangyu1996@gmail.com>
2018-02-03 20:29:27 -0500

    ~

---
+++ 162.py
@@ -0,0 +1,18 @@
+# Freddy @DP
+# Feb. 3rd, 2018
+
+class Solution:
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums)-1
+        while (l < r):
+            mid = l+(r-l)//2
+            if (nums[mid] > nums[mid+1]):
+                r = mid
+            else:
+                l = mid+1
+            
+        return l
